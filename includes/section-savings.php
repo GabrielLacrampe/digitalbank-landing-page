eacbcea24de16d8a188ca5d31146a5f8a51e9f75
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-20 10:32:18 +0100

    Section Savings to PHP and JSON

--- includes/section-savings.php
+++ includes/section-savings.php
@@ -1,15 +1,37 @@
-<h1>Life, meet savings</h1>
-<img class="placeholder-image" src="../assets/media/<?php echo $contenido[2]['nombre_archivo']; ?>" alt="<?php echo $contenido[2]['titulo']; ?>">
-<p> 
-Grow your money with up to 4% AER (variable)¹ interest rate on Savings, paid every day.
-AER stands for Annual Equivalent Rate and illustrates what the interest rate would be if interest was paid and compounded once each year. The interest rate is variable and subject to your selected plan. T&Cs apply.
-</p>
-<div>
-    <div class="button-style hidden-mobile"><a href=""><span>Weading</span></a></div>
-</div>
-<div>
-    <div class="button-style hidden-mobile"><a href=""><span>Adventure</span></a></div>
-</div>
-<div>
-    <div class="button-style hidden-mobile"><a href=""><span>Moving</span></a></div>
-</div>
+<?php 
+// Accedemos directamente a la sección que nos interesa mediante su llave
+$mi_seccion = $web_data['section-savings']; 
+$datos = $mi_seccion['contenido']; 
+?>
+
+<section id="savings">
+    <!-- Define el estado inicial  -->
+    <?php $inicial = $datos['seccion_config']['estado_inicial']; ?>
+
+    <h1 class="static-title"><?php echo $datos['bloques_fijos']['title']; ?></h1>
+    
+    <!-- Sentencia para instanciar imagenes que pueden variar dependiendo de la opcion activa  -->
+    <img class="dinamic-img placeholder-image" src="../assets/media/<?php echo $datos['bloques_variables'][$inicial]['imagen']; ?>" alt="<?php echo $datos['bloques_variables'][$inicial]['titulo']; ?>">
+    
+    <p class="static-text"><?php echo $datos['bloques_fijos']['description']; ?></p>
+    
+    <p class="dinamic-text placeholder-text"><?php echo $datos['bloques_variables'][$inicial]['texto']; ?></p>
+
+    <div>
+        <div class="button-style hidden-mobile"><a href="<?php echo $datos['bloques_fijos']['boton_exploreSavings']['url']; ?>"><span><?php echo $datos['bloques_fijos']['boton_exploreSavings']['texto']; ?></span></a></div>  
+    </div>
+    
+    <!-- Crea los botones extraidos desde el JSON-->
+    <div class="tabs">
+        <?php foreach ($datos['controles'] as $btn): ?>
+            <button class="tab-btn" data-target="<?php echo $btn['target_data']; ?>">
+                <?php echo $btn['label']; ?>
+            </button>
+        <?php endforeach; ?>
+    </div>
+
+     <!-- Necesario para que funcionen los botones-->
+    <script class="data-json" type="application/json">
+        <?php echo json_encode($datos['bloques_variables']); ?>
+    </script>
+</section>
