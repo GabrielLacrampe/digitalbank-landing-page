23b4c2828586b52a4c68f86f3251e4cba31942a8
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-20 14:14:54 +0100

    Update section cards

--- includes/section-cards.php
+++ includes/section-cards.php
@@ -6,22 +6,22 @@ $datos = $mi_seccion['contenido'];
 
 <section id="cards">
     <!-- Define el estado inicial  -->
-    <?php $inicial = $datos['seccion_config']['estado_inicial']; ?>
+    <?php $inicial = $datos['section_config']['initial_state']; ?>
 
-    <h1 class="dinamic-title"><?php echo $datos['bloques_variables'][$inicial]['titulo']; ?></h1>
+    <h1 class="dynamic-title"><?php echo $datos['dynamic_blocks'][$inicial]['title']; ?></h1>
     
     <!-- Sentencia para instanciar imagenes que pueden variar dependiendo de la opcion activa  -->
-    <img class="dinamic-img placeholder-image" src="../assets/media/<?php echo $datos['bloques_variables'][$inicial]['imagen']; ?>" alt="<?php echo $datos['bloques_variables'][$inicial]['titulo']; ?>">
+    <img class="dynamic-img placeholder-image" src="../assets/media/<?php echo $datos['dynamic_blocks'][$inicial]['image']; ?>" alt="<?php echo $datos['dynamic_blocks'][$inicial]['alt']; ?>">
         
-    <p class="dinamic-text placeholder-text"><?php echo $datos['bloques_variables'][$inicial]['texto']; ?></p>
+    <p class="dynamic-description placeholder-text"><?php echo $datos['dynamic_blocks'][$inicial]['description']; ?></p>
 
     <div>
-        <div class="dinamic-button button-style hidden-mobile"><a href="<?php echo $datos['bloques_variables'][$inicial]['button']['url']; ?>"><span><?php echo $datos['bloques_variables'][$inicial]['button']['texto_button']; ?></span></a></div>  
+        <div class="dynamic-button button-style hidden-mobile"><a href="<?php echo $datos['dynamic_blocks'][$inicial]['button']['url']; ?>"><span><?php echo $datos['dynamic_blocks'][$inicial]['button']['button_text']; ?></span></a></div>  
     </div>
     
     <!-- Crea los botones extraidos desde el JSON-->
     <div class="tabs">
-        <?php foreach ($datos['controles'] as $btn): ?>
+        <?php foreach ($datos['controllers'] as $btn): ?>
             <button class="tab-btn" data-target="<?php echo $btn['target_data']; ?>">
                 <?php echo $btn['label']; ?>
             </button>
@@ -30,6 +30,6 @@ $datos = $mi_seccion['contenido'];
 
      <!-- Necesario para que funcionen los botones-->
     <script class="data-json" type="application/json">
-        <?php echo json_encode($datos['bloques_variables']); ?>
+        <?php echo json_encode($datos['dynamic_blocks']); ?>
     </script>
 </section>
