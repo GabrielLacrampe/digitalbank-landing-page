b08d275da5ef415fc75ea85159097e08c0689375
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-20 12:09:48 +0100

    Update section-cards.php

--- includes/section-cards.php
+++ includes/section-cards.php
@@ -1,12 +1,35 @@
-<h1>Elevate your spend</h1>
-<img class="placeholder-image" src="../assets/media/<?php echo $contenido[5]['nombre_archivo']; ?>" alt="<?php echo $contenido[5]['titulo']; ?>">
-<p> 
-Earn points on your purchases with one of our debit cards. Then redeem them for Airline Miles and more. RevPoints T&Cs apply.
-Some cards available on paid plans only. Fees may apply.
-</p>
-<div>
-    <div class="button-style hidden-mobile"><a href=""><span>Physical Card</span></a></div>
-</div>
-<div>
-    <div class="button-style hidden-mobile"><a href=""><span>Virtual Card</span></a></div>
-</div>
+<?php 
+// Accedemos directamente a la sección que nos interesa mediante su llave
+$mi_seccion = $web_data['section-cards']; 
+$datos = $mi_seccion['contenido']; 
+?>
+
+<section id="cards">
+    <!-- Define el estado inicial  -->
+    <?php $inicial = $datos['seccion_config']['estado_inicial']; ?>
+
+    <h1 class="dinamic-title"><?php echo $datos['bloques_variables'][$inicial]['titulo']; ?></h1>
+    
+    <!-- Sentencia para instanciar imagenes que pueden variar dependiendo de la opcion activa  -->
+    <img class="dinamic-img placeholder-image" src="../assets/media/<?php echo $datos['bloques_variables'][$inicial]['imagen']; ?>" alt="<?php echo $datos['bloques_variables'][$inicial]['titulo']; ?>">
+        
+    <p class="dinamic-text placeholder-text"><?php echo $datos['bloques_variables'][$inicial]['texto']; ?></p>
+
+    <div>
+        <div class="dinamic-button button-style hidden-mobile"><a href="<?php echo $datos['bloques_variables'][$inicial]['button']['url']; ?>"><span><?php echo $datos['bloques_variables'][$inicial]['button']['texto_button']; ?></span></a></div>  
+    </div>
+    
+    <!-- Crea los botones extraidos desde el JSON-->
+    <div class="tabs">
+        <?php foreach ($datos['controles'] as $btn): ?>
+            <button class="tab-btn" data-target="<?php echo $btn['target_data']; ?>">
+                <?php echo $btn['label']; ?>
+            </button>
+        <?php endforeach; ?>
+    </div>
+
+     <!-- Necesario para que funcionen los botones-->
+    <script class="data-json" type="application/json">
+        <?php echo json_encode($datos['bloques_variables']); ?>
+    </script>
+</section>
