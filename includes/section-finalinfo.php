32905b6f6084b5134c3931a06eee4b317a0e5a24
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-20 11:34:12 +0100

    Update section-finalinfo.php

--- includes/section-finalinfo.php
+++ includes/section-finalinfo.php
@@ -1,22 +1,19 @@
-<h1>Únete a los Más de 70+ millones de personas que ya utilizan Revolut</h1>
-<div>
-    <div class="button-style hidden-mobile"><a href=""><span>Download</span></a></div>
-</div>
-<p>
-¹The Annual Equivalent Rate (AER) shows the interest you can earn over 1 year. AER is compounded, so you’ll earn interest on interest already earned. Rates depend on your plan type and savings' currency, from up to 3% on our Standard plan to up to 4% on our Ultra plan. Paid plan subscription fees and T&Cs apply. Interest offered is subject to change and any interest earned is liable to the applicable taxes. Instant Access Savings T&Cs apply.
+<?php 
+// Accedemos directamente a la sección que nos interesa mediante su llave
+$mi_seccion = $web_data['section-finalinfo']; 
+$datos = $mi_seccion['contenido']; 
+?>
 
-²FOR STOCK TRADING: Capital at risk.
+<section id="finalinfo">
 
-Revolut Trading Ltd provides a non-advised execution-only service in shares. Revolut Trading Ltd does not provide investment advice or personal recommendations. You, as an individual investor, must make your own decisions, seeking independent professional advice if you are unsure as to the suitability or appropriateness of any investment for your individual circumstances or needs.
+    <h1 class="static-title"><?php echo $datos['bloques_fijos']['title']; ?></h1>
+    
+    <img class="placeholder-image" src="../assets/media/<?php echo $datos['bloques_fijos']['imagen']; ?>" alt="<?php echo $datos['bloques_fijos']['title']; ?>">
+    
+    <p class="static-text"><?php echo $datos['bloques_fijos']['description']; ?></p>
 
-The value of investments can go up as well as down and you may receive less than your original investment or lose the value of your entire initial investment. Past performance and forecasts are not reliable indicators of future results. Currency rate fluctuations can adversely impact the overall returns on your original investment. Any trades outside of your monthly allowance are charged at 0.25% of the order amount if you are a Standard, Plus, Premium, or Metal customer, or at 0.12% of the order amount if you are an Ultra/Trading Pro customer. Read more on these fees. Further information about the investment service provided by Revolut Trading Ltd can be found in the Terms of Business, Risk Disclosure, and Invest FAQs.
+    <div>
+        <div class="button-style hidden-mobile"><a href="<?php echo $datos['bloques_fijos']['botton']['url']; ?>"><span><?php echo $datos['bloques_fijos']['botton']['texto']; ?></span></a></div>
+    </div>
 
-Revolut Trading Ltd (No. 11567840) is a firm authorised and regulated by the Financial Conduct Authority (FRN: 933846). The registered address of Revolut Trading Ltd is at 30 South Colonnade, London, United Kingdom, E14 5HX.
-
-³FOR COMMODITIES: Capital at risk.
-Revolut Ltd commodities service is not regulated by the FCA and it is not protected or covered by the Financial Ombudsman Service, or the Financial Services Compensation Scheme.
-
-Revolut Ltd (No. 08804411) is also authorised by the FCA under the Electronic Money Regulations 2011 (Firm Reference 900562). Insurance related-products are arranged by Revolut Travel Ltd which is authorised by the FCA to undertake insurance distribution activities (FCA No: 780586) and by Revolut Ltd, an Appointed Representative of Revolut Travel Ltd in relation to insurance distribution activities. 
-
-If you would like to find out more about which Revolut entity you receive services from, or if you have any other questions, please reach out to us via the in-app chat in the Revolut app.
-</p>
+</section>
