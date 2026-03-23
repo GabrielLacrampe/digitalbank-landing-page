b66714200ab7b76e4765797f7f1bfc43ee6a1d91
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-23 12:37:48 +0100

    Section Hero to JSON

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -1,14 +1,17 @@
+<?php 
+// Accedemos directamente a la sección que nos interesa mediante su llave
+$mi_seccion = $web_data['section-hero']; 
+$datos = $mi_seccion['contenido']; 
+?>
 <div class="contenido-hero testing-box">
-   <img src="../assets/media/<?php echo $contenido[3]['nombre_archivo']; ?>" class="fondo-animado" alt="<?php echo $contenido[3]['titulo']; ?>">
+    <img class="fondo-animado" src="../assets/media/<?php echo $datos['static_blocks']['images']['image_a']['url']; ?>" alt="<?php echo $datos['static_blocks']['images']['image_a']['alt']; ?>">
     <div class="testing-box hero-30"></div>
         <div class="testing-box hero-content-interior">
-            <h1 class="hero-content-title color-white">Banca y mucho más</h1>
-            <p class="hero-content-paragraph color-white">Tanto si estás en casa como de viaje,
-                deja que Revolut supere tus expectativas bancarias.
-                Regístrate con un toque.
+            <h1 class="hero-content-title color-white"><?php echo $datos['static_blocks']['group_a']['title']; ?></h1>
+            <p class="hero-content-paragraph color-white"><?php echo $datos['static_blocks']['group_a']['description']; ?>
             </p>
             <div class="button-style hero-content-button">
-                <a class="color-white" href=""> <span>Descargar App</span></a>
+                <a class="color-white" href="<?php echo $datos['static_blocks']['group_a']['button']['url']; ?>"> <span><?php echo $datos['static_blocks']['group_a']['button']['button_text']; ?></span></a>
             </div>
         </div>
     <div class="testing-box hero-30"></div>     
