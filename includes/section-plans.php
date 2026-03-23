893cf2f2e6a1ef1bc27f3bd392a4fe9a04642166
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-23 11:57:48 +0100

    Section Plans to JSON

--- includes/section-plans.php
+++ includes/section-plans.php
@@ -1,70 +1,66 @@
-<h2 style="color: white;">Elige tu plan</h2>
+<?php 
+// Accedemos directamente a la sección que nos interesa mediante su llave
+$mi_seccion = $web_data['section-plans']; 
+$datos = $mi_seccion['contenido']; 
+?>
 <section id="section-plans">
+    <h2 class="static-title"><?php echo $datos['static_blocks']['title']; ?></h2>
     <ul class="plan-container">
         <li>
-            <a href="">
+            <a href="<?php echo $datos['static_blocks']['plans']['plan_standard']['url']; ?>">
                 <div class="plan-style">
                     <span>
-                        <h2>Estándar</h2>
+                        <h2><?php echo $datos['static_blocks']['plans']['plan_standard']['title']; ?></h2>
                     </span>
-                    <h3>Gratis</h3>
-                    <p>Para las finanzas básicas: todo lo que necesitas para gestionar tu dinero de forma
-                        sencilla y eficaz. Enviar dinero al extranjero o ajustarte a un presupuesto nunca ha
-                        sido tan fácil.</p>
+                    <h3><?php echo $datos['static_blocks']['plans']['plan_standard']['price']; ?></h3>
+                    <p><?php echo $datos['static_blocks']['plans']['plan_standard']['description']; ?></p>
                 </div>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="<?php echo $datos['static_blocks']['plans']['plan_plus']['url']; ?>">
                 <div class="plan-style">
                     <span>
-                        <h2>Estándar</h2>
+                        <h2><?php echo $datos['static_blocks']['plans']['plan_plus']['title']; ?></h2>
                     </span>
-                    <h3>Gratis</h3>
-                    <p>Para las finanzas básicas: todo lo que necesitas para gestionar tu dinero de forma
-                        sencilla y eficaz. Enviar dinero al extranjero o ajustarte a un presupuesto nunca ha
-                        sido tan fácil.</p>
+                    <h3><?php echo $datos['static_blocks']['plans']['plan_plus']['price']; ?></h3>
+                    <p><?php echo $datos['static_blocks']['plans']['plan_plus']['description']; ?></p>
                 </div>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="<?php echo $datos['static_blocks']['plans']['plan_premium']['url']; ?>">
                 <div class="plan-style">
                     <span>
-                        <h2>Estándar</h2>
+                        <h2><?php echo $datos['static_blocks']['plans']['plan_premium']['title']; ?></h2>
                     </span>
-                    <h3>Gratis</h3>
-                    <p>Para las finanzas básicas: todo lo que necesitas para gestionar tu dinero de forma
-                        sencilla y eficaz. Enviar dinero al extranjero o ajustarte a un presupuesto nunca ha
-                        sido tan fácil.</p>
+                    <h3><?php echo $datos['static_blocks']['plans']['plan_premium']['price']; ?></h3>
+                    <p><?php echo $datos['static_blocks']['plans']['plan_premium']['description']; ?></p>
                 </div>
             </a>
         </li>
     </ul>
+
     <ul class="plan-container">
-        <li>
-            <a href="">
+       <li>
+            <a href="<?php echo $datos['static_blocks']['plans']['plan_metal']['url']; ?>">
                 <div class="plan-style">
                     <span>
-                        <h2>Estándar</h2>
+                        <h2><?php echo $datos['static_blocks']['plans']['plan_metal']['title']; ?></h2>
                     </span>
-                    <h3>Gratis</h3>
-                    <p>Para las finanzas básicas: todo lo que necesitas para gestionar tu dinero de forma
-                        sencilla y eficaz. Enviar dinero al extranjero o ajustarte a un presupuesto nunca ha
-                        sido tan fácil.</p>
+                    <h3><?php echo $datos['static_blocks']['plans']['plan_metal']['price']; ?></h3>
+                    <p><?php echo $datos['static_blocks']['plans']['plan_metal']['description']; ?></p>
                 </div>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="<?php echo $datos['static_blocks']['plans']['plan_ultra']['url']; ?>">
                 <div class="plan-style">
                     <span>
-                        <h2>Estándar</h2>
+                        <h2><?php echo $datos['static_blocks']['plans']['plan_ultra']['title']; ?></h2>
                     </span>
-                    <h3>Gratis</h3>
-                    <p>Para las finanzas básicas: todo lo que necesitas para gestionar tu dinero de forma
-                        sencilla y eficaz. Enviar dinero al extranjero o ajustarte a un presupuesto nunca ha
-                        sido tan fácil.</p>
+                    <h3><?php echo $datos['static_blocks']['plans']['plan_ultra']['price']; ?></h3>
+                    <p><?php echo $datos['static_blocks']['plans']['plan_ultra']['description']; ?></p>
                 </div>
             </a>
         </li>
