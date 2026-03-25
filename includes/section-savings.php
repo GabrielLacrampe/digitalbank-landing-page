6d1dffb617ebb7c6a34935b0c26aed87240e984f
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-25 17:32:13 +0100

    Update section-savings.php

--- includes/section-savings.php
+++ includes/section-savings.php
@@ -8,18 +8,26 @@ $datos = $mi_seccion['contenido'];
         <?php $inicial = $datos['section_config']['initial_state']; ?>
         
         <h1 class="static-title"><?php echo $datos['static_blocks']['title']; ?></h1>
-        
+
         <?php renderImage($datos['dynamic_blocks'][$inicial], true); ?>
         
-        <p class="static-text"><?php echo $datos['static_blocks']['description']; ?></p>
+        <?php renderText($datos['static_blocks'], false); ?>
         
-        <p class="dynamic-text"><?php echo $datos['dynamic_blocks'][$inicial]['description']; ?></p>
-        
+        <h1 class="dynamic-title"><?php echo $datos['dynamic_blocks'][$inicial]['title']; ?></h1>
+
+        <?php renderText($datos['dynamic_blocks'][$inicial], true); ?>
+
         <div>
             <?php renderButton($datos['static_blocks']['button'], false, 'hidden-mobile'); ?>
         </div>
 
-        <?php renderControllers($datos['controllers']); ?>      
+        <div class="tabs">
+            <?php foreach ($datos['controllers'] as $btn): ?>
+                <button class="tab-btn" data-target="<?php echo $btn['target_data']; ?>">
+                    <?php echo $btn['label']; ?>
+                </button>
+            <?php endforeach; ?>
+        </div>   
         <script class="data-json" type="application/json">
             <?php echo json_encode($datos['dynamic_blocks']); ?>
         </script>
