a70f840aef1433e75cea147d9a6aed219c1dc7e8
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-25 17:31:49 +0100

    Update section-awards&references.php

--- includes/section-awards&references.php
+++ includes/section-awards&references.php
@@ -7,28 +7,24 @@
     <section id="awards-references">
         <h1 class="static-title"><?php echo $datos['static_blocks']['title']; ?></h1>
         <div> 
-            <p class="static-text"><?php echo $datos['static_blocks']['award_a']['description']; ?></p>
+            <?php renderText($datos['static_blocks']['award_a'], false); ?>
             <?php renderImage($datos['static_blocks']['award_a'], false); ?>
         </div>
         <div> 
-            <p class="static-text"><?php echo $datos['static_blocks']['award_b']['description']; ?></p>
+            <?php renderText($datos['static_blocks']['award_b'], false); ?>
             <?php renderImage($datos['static_blocks']['award_b'], false); ?>
         </div>
         <div> 
-            <p class="static-text"><?php echo $datos['static_blocks']['award_c']['description']; ?></p>
+            <?php renderText($datos['static_blocks']['award_c'], false); ?>
             <?php renderImage($datos['static_blocks']['award_c'], false); ?>
         </div>
         <div> 
-            <p class="static-text"><?php echo $datos['static_blocks']['award_d']['description']; ?></p>
+            <?php renderText($datos['static_blocks']['award_d'], false); ?>
             <?php renderImage($datos['static_blocks']['award_d'], false); ?>
         </div>
         <div> 
-            <p class="static-text"><?php echo $datos['static_blocks']['award_e']['description']; ?></p>
+            <?php renderText($datos['static_blocks']['award_e'], false); ?>
             <?php renderImage($datos['static_blocks']['award_e'], false); ?>
-        </div>
-        <div> 
-            <p class="static-text"><?php echo $datos['static_blocks']['award_f']['description']; ?></p>
-            <?php renderImage($datos['static_blocks']['award_f'], false); ?>
         </div>
     </section>
 </div>
