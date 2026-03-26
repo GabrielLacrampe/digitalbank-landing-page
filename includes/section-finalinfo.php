7fe3808a1e26742cdb0dcad8e7c40ae90fc6818f
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 11:47:16 +0100

    Update section-finalinfo.php

--- includes/section-finalinfo.php
+++ includes/section-finalinfo.php
@@ -1,17 +1,13 @@
 <?php 
-// Accedemos directamente a la sección que nos interesa mediante su llave
 $mi_seccion = $web_data['section-finalinfo']; 
 $datos = $mi_seccion['contenido']; 
 ?>
-<div class="testing-box">
-    <section id="finalinfo">
-        <?php renderTitle($datos['static_blocks'], false); ?>
+<section id="finalinfo">
+    <?php renderTitle($datos['static_blocks'], false); ?>
 
-        <?php renderImage($datos['static_blocks'], false); ?>
+    <?php renderImage($datos['static_blocks'], false); ?>
 
-        <?php renderText($datos['static_blocks'], false); ?>
-        <div>
-            <?php renderButton($datos['static_blocks']['button'], false); ?>
-        </div>
-    </section>
-</div>
+    <?php renderText($datos['static_blocks'], false); ?>
+
+    <?php renderButton($datos['static_blocks']['button'], false); ?>
+</section>
