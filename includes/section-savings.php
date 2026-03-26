55ce90d8d48fcce653d054adaef3e0fcecd6874f
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 11:47:33 +0100

    Update section-savings.php

--- includes/section-savings.php
+++ includes/section-savings.php
@@ -1,35 +1,23 @@
 <?php 
-// Accedemos directamente a la sección que nos interesa mediante su llave
 $mi_seccion = $web_data['section-savings']; 
 $datos = $mi_seccion['contenido']; 
+$inicial = $datos['section_config']['initial_state'];
 ?>
-<div class="testing-box">
-    <section id="savings">
-        <?php $inicial = $datos['section_config']['initial_state']; ?>
-        
-        <?php renderTitle($datos['static_blocks'], false); ?>
+<section id="savings">        
+    <?php renderTitle($datos['static_blocks'], false); ?>
 
-        <?php renderImage($datos['dynamic_blocks'][$inicial], true); ?>
-        
-        <?php renderText($datos['static_blocks'], false); ?>
-        
-        <?php renderTitle($datos['dynamic_blocks'][$inicial], true); ?>
+    <?php renderImage($datos['dynamic_blocks'][$inicial], true); ?>
+    
+    <?php renderText($datos['static_blocks'], false); ?>
+    
+    <?php renderTitle($datos['dynamic_blocks'][$inicial], true); ?>
 
-        <?php renderText($datos['dynamic_blocks'][$inicial], true); ?>
+    <?php renderText($datos['dynamic_blocks'][$inicial], true); ?>
 
-        <div>
-            <?php renderButton($datos['static_blocks']['button'], false, 'hidden-mobile'); ?>
-        </div>
+    <?php renderButton($datos['static_blocks']['button'], false, 'hidden-mobile'); ?>
 
-        <div class="tabs">
-            <?php foreach ($datos['controllers'] as $btn): ?>
-                <button class="tab-btn" data-target="<?php echo $btn['target_data']; ?>">
-                    <?php echo $btn['label']; ?>
-                </button>
-            <?php endforeach; ?>
-        </div>   
-        <script class="data-json" type="application/json">
-            <?php echo json_encode($datos['dynamic_blocks']); ?>
-        </script>
-    </section>
-</div>
+    <?php renderControllers($datos['controllers']); ?>
+    <script class="data-json" type="application/json">
+        <?php echo json_encode($datos['dynamic_blocks']); ?>
+    </script>
+</section>
