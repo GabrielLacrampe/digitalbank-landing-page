da3773a70973b031f397ef76266b151cba3e29b4
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 11:47:13 +0100

    Update section-cards.php

--- includes/section-cards.php
+++ includes/section-cards.php
@@ -2,24 +2,19 @@
     // Accedemos directamente a la sección que nos interesa mediante su llave
     $mi_seccion = $web_data['section-cards']; 
     $datos = $mi_seccion['contenido']; 
+     $inicial = $datos['section_config']['initial_state'];
 ?>
-<div class="testing-box">
-    <section id="cards">
-        <?php $inicial = $datos['section_config']['initial_state']; ?>
-        
-        <?php renderTitle($datos['dynamic_blocks'][$inicial], true); ?>
+<section id="cards">        
+    <?php renderTitle($datos['dynamic_blocks'][$inicial], true); ?>
 
-        <?php renderImage($datos['dynamic_blocks'][$inicial], true); ?>
+    <?php renderImage($datos['dynamic_blocks'][$inicial], true); ?>
 
-        <?php renderText($datos['dynamic_blocks'][$inicial], true); ?>
+    <?php renderText($datos['dynamic_blocks'][$inicial], true); ?>
 
-        <div>
-            <?php renderButton($datos['dynamic_blocks'][$inicial]['button'], true, 'hidden-desktop'); ?>
-        </div>
+    <?php renderButton($datos['dynamic_blocks'][$inicial]['button'], true, 'hidden-desktop'); ?>
 
-        <?php renderControllers($datos['controllers']); ?>
-        <script class="data-json" type="application/json">
-            <?php echo json_encode($datos['dynamic_blocks']); ?>
-        </script>
-    </section>
-</div>
+    <?php renderControllers($datos['controllers']); ?>
+    <script class="data-json" type="application/json">
+        <?php echo json_encode($datos['dynamic_blocks']); ?>
+    </script>
+</section>
