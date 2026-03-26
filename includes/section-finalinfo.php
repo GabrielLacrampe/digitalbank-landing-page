98409d8efb91276933d36877f8bbe82cdae453ee
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 10:54:34 +0100

    Update section-finalinfo.php

--- includes/section-finalinfo.php
+++ includes/section-finalinfo.php
@@ -5,7 +5,7 @@ $datos = $mi_seccion['contenido'];
 ?>
 <div class="testing-box">
     <section id="finalinfo">
-        <h1 class="static-title"><?php echo $datos['static_blocks']['title']; ?></h1>
+        <?php renderTitle($datos['static_blocks'], false); ?>
 
         <?php renderImage($datos['static_blocks'], false); ?>
 
