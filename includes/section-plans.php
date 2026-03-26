06d9fb4b2693b6bdc9bd7aad54a87d85a88ffe22
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 12:01:15 +0100

    Update section-plans.php

--- includes/section-plans.php
+++ includes/section-plans.php
@@ -7,62 +7,22 @@ $datos = $mi_seccion['contenido'];
     <h2 class="static-title"><?php echo $datos['static_blocks']['title']; ?></h2>
     <ul class="plan-container">
         <li>
-            <a href="<?php echo $datos['static_blocks']['plans']['plan_standard']['url']; ?>">
-                <div class="plan-style">
-                    <span>
-                        <h2><?php echo $datos['static_blocks']['plans']['plan_standard']['title']; ?></h2>
-                    </span>
-                    <h3><?php echo $datos['static_blocks']['plans']['plan_standard']['price']; ?></h3>
-                    <p><?php echo $datos['static_blocks']['plans']['plan_standard']['description']; ?></p>
-                </div>
-            </a>
+            <?php renderPlan($datos['static_blocks']['plans']['plan_standard']); ?>
         </li>
         <li>
-            <a href="<?php echo $datos['static_blocks']['plans']['plan_plus']['url']; ?>">
-                <div class="plan-style">
-                    <span>
-                        <h2><?php echo $datos['static_blocks']['plans']['plan_plus']['title']; ?></h2>
-                    </span>
-                    <h3><?php echo $datos['static_blocks']['plans']['plan_plus']['price']; ?></h3>
-                    <p><?php echo $datos['static_blocks']['plans']['plan_plus']['description']; ?></p>
-                </div>
-            </a>
+            <?php renderPlan($datos['static_blocks']['plans']['plan_plus']); ?>
         </li>
         <li>
-            <a href="<?php echo $datos['static_blocks']['plans']['plan_premium']['url']; ?>">
-                <div class="plan-style">
-                    <span>
-                        <h2><?php echo $datos['static_blocks']['plans']['plan_premium']['title']; ?></h2>
-                    </span>
-                    <h3><?php echo $datos['static_blocks']['plans']['plan_premium']['price']; ?></h3>
-                    <p><?php echo $datos['static_blocks']['plans']['plan_premium']['description']; ?></p>
-                </div>
-            </a>
+            <?php renderPlan($datos['static_blocks']['plans']['plan_premium']); ?>
         </li>
     </ul>
 
     <ul class="plan-container">
        <li>
-            <a href="<?php echo $datos['static_blocks']['plans']['plan_metal']['url']; ?>">
-                <div class="plan-style">
-                    <span>
-                        <h2><?php echo $datos['static_blocks']['plans']['plan_metal']['title']; ?></h2>
-                    </span>
-                    <h3><?php echo $datos['static_blocks']['plans']['plan_metal']['price']; ?></h3>
-                    <p><?php echo $datos['static_blocks']['plans']['plan_metal']['description']; ?></p>
-                </div>
-            </a>
+            <?php renderPlan($datos['static_blocks']['plans']['plan_metal']); ?>
         </li>
         <li>
-            <a href="<?php echo $datos['static_blocks']['plans']['plan_ultra']['url']; ?>">
-                <div class="plan-style">
-                    <span>
-                        <h2><?php echo $datos['static_blocks']['plans']['plan_ultra']['title']; ?></h2>
-                    </span>
-                    <h3><?php echo $datos['static_blocks']['plans']['plan_ultra']['price']; ?></h3>
-                    <p><?php echo $datos['static_blocks']['plans']['plan_ultra']['description']; ?></p>
-                </div>
-            </a>
+            <?php renderPlan($datos['static_blocks']['plans']['plan_ultra']); ?>
         </li>
     </ul>
 </section>
