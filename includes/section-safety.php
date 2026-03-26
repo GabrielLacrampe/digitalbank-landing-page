528570e0f35b017fc1bd14d3800fb62035d99328
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 11:47:27 +0100

    Update section-safety.php

--- includes/section-safety.php
+++ includes/section-safety.php
@@ -1,18 +1,13 @@
 <?php 
-// Accedemos directamente a la sección que nos interesa mediante su llave
 $mi_seccion = $web_data['section-safety']; 
 $datos = $mi_seccion['contenido']; 
 ?>
-<div class="testing-box">
-    <section id="safety">
-        <?php renderTitle($datos['static_blocks'], false); ?>
+<section id="safety">
+    <?php renderTitle($datos['static_blocks'], false); ?>
 
-        <?php renderImage($datos['static_blocks'], false); ?>
+    <?php renderImage($datos['static_blocks'], false); ?>
 
-        <?php renderText($datos['static_blocks'], false); ?>
-
-        <div>
-            <?php renderButton($datos['static_blocks']['button'], false); ?>
-        </div>
-    </section>
-</div>
+    <?php renderText($datos['static_blocks'], false); ?>
+    
+    <?php renderButton($datos['static_blocks']['button'], false); ?>
+</section>
