32e7cbafc8c9f9fe17625c31ec7bffe39e68b2cc
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 11:47:20 +0100

    Update section-hero.php

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -1,16 +1,19 @@
 <?php 
-// Accedemos directamente a la sección que nos interesa mediante su llave
 $mi_seccion = $web_data['section-hero']; 
 $datos = $mi_seccion['contenido']; 
 ?>
-<div class="contenido-hero testing-box">
+<div class="contenido-hero">
     <?php renderImage($datos['static_blocks']['images']['image_a'], false, 'fondo-animado'); ?>
-    <div class="testing-box hero-30"></div>
-        <div class="testing-box hero-content-interior">
-            <?php renderTitle($datos['static_blocks']['group_a'], false, 'hero-content-title'); ?>
+    
+    <div class="hero-30"></div>
 
-            <?php renderText($datos['static_blocks']['group_a'], false, 'hero-content-paragraph'); ?>
-            <?php renderButton($datos['static_blocks']['group_a']['button'], false, 'hero-content-button'); ?>
-        </div>
-    <div class="testing-box hero-30"></div>     
+    <div class="hero-content-interior">
+        <?php renderTitle($datos['static_blocks']['group_a'], false, 'hero-content-title'); ?>
+
+        <?php renderText($datos['static_blocks']['group_a'], false, 'hero-content-paragraph'); ?>
+        
+        <?php renderButton($datos['static_blocks']['group_a']['button'], false, 'hero-content-button'); ?>
+    </div>
+
+    <div class="hero-30"></div>     
 </div>
