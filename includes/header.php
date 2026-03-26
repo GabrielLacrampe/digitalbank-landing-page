442ac607b0fc411afa63e7c48ba6c55adbcaca04
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 11:47:03 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,18 +2,14 @@
 $mi_seccion = $web_data['header']; 
 $datos = $mi_seccion['contenido']; 
 ?>
-
 <div class="horizontal-container">
-    <div class="">
+    <div>
         <?php renderImage($datos['static_blocks'], false, 'header-logo'); ?>
     </div>
 
     <div class="horizontal-container width100">
         <span class="horizontal-container">
-            <?php 
-                // TODO: este metodo no me termina
-                renderAllButtons($datos['static_blocks'], false);
-            ?>
+            <?php renderAllButtons($datos['static_blocks'], false); ?>
         </span>
     </div>
     <div>
