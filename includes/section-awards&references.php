c79ebd6aa178e76e63522ec478c255b908836eee
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 11:47:06 +0100

    Update section-awards&references.php

--- includes/section-awards&references.php
+++ includes/section-awards&references.php
@@ -3,28 +3,21 @@
     $mi_seccion = $web_data['section-awards_references']; 
     $datos = $mi_seccion['contenido']; 
 ?>
-<div class="testing-box">
-    <section id="awards-references">
-        <?php renderTitle($datos['static_blocks'], false); ?>
-        <div> 
-            <?php renderText($datos['static_blocks']['award_a'], false); ?>
-            <?php renderImage($datos['static_blocks']['award_a'], false); ?>
-        </div>
-        <div> 
-            <?php renderText($datos['static_blocks']['award_b'], false); ?>
-            <?php renderImage($datos['static_blocks']['award_b'], false); ?>
-        </div>
-        <div> 
-            <?php renderText($datos['static_blocks']['award_c'], false); ?>
-            <?php renderImage($datos['static_blocks']['award_c'], false); ?>
-        </div>
-        <div> 
-            <?php renderText($datos['static_blocks']['award_d'], false); ?>
-            <?php renderImage($datos['static_blocks']['award_d'], false); ?>
-        </div>
-        <div> 
-            <?php renderText($datos['static_blocks']['award_e'], false); ?>
-            <?php renderImage($datos['static_blocks']['award_e'], false); ?>
-        </div>
-    </section>
-</div>
+<section id="awards-references">
+    <?php renderTitle($datos['static_blocks'], false); ?>
+    
+    <?php renderText($datos['static_blocks']['award_a'], false); ?>
+    <?php renderImage($datos['static_blocks']['award_a'], false); ?>
+
+    <?php renderText($datos['static_blocks']['award_b'], false); ?>
+    <?php renderImage($datos['static_blocks']['award_b'], false); ?>
+
+    <?php renderText($datos['static_blocks']['award_c'], false); ?>
+    <?php renderImage($datos['static_blocks']['award_c'], false); ?>
+    
+    <?php renderText($datos['static_blocks']['award_d'], false); ?>
+    <?php renderImage($datos['static_blocks']['award_d'], false); ?>
+    
+    <?php renderText($datos['static_blocks']['award_e'], false); ?>
+    <?php renderImage($datos['static_blocks']['award_e'], false); ?>
+</section>
