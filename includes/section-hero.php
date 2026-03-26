852ca111b8d7b1e535773ee4dc8d92476661120d
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 12:54:44 +0100

    Update section-hero.php

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -6,13 +6,25 @@ $datos = $mi_seccion['contenido'];
     <?php renderImage($datos['static_blocks']['images']['image_a'], false, 'fondo-animado'); ?>
     
     <div class="hero-30"></div>
-
-    <div class="hero-content-interior">
+    
+    <div class="hero-content-exterior">
         <?php renderTitle($datos['static_blocks']['group_a'], false, 'hero-content-title'); ?>
 
         <?php renderText($datos['static_blocks']['group_a'], false, 'hero-content-paragraph'); ?>
         
         <?php renderButton($datos['static_blocks']['group_a']['button'], false, 'hero-content-button'); ?>
+    </div>
+
+    <div class="hero-content-interior">
+        <?php renderTitle($datos['static_blocks']['group_b'], false, 'hero-content-title'); ?>
+
+        <?php renderText($datos['static_blocks']['group_b'], false, 'hero-content-paragraph'); ?>
+
+        <?php renderImage($datos['static_blocks']['images']['image_b'], false, 'header-logo'); ?>
+
+        <?php renderImage($datos['static_blocks']['images']['image_c'], false, 'header-logo'); ?>
+
+        <?php renderButton($datos['static_blocks']['group_b']['button'], false, 'hero-content-button'); ?>
     </div>
 
     <div class="hero-30"></div>     
