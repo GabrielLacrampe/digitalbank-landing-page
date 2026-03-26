1b274c6d659e49c97f41bb4c9e570544deeccb78
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 12:06:52 +0100

    Update section-plans.php

--- includes/section-plans.php
+++ includes/section-plans.php
@@ -1,28 +1,18 @@
-<?php 
+<?php
 // Accedemos directamente a la sección que nos interesa mediante su llave
-$mi_seccion = $web_data['section-plans']; 
-$datos = $mi_seccion['contenido']; 
+$mi_seccion = $web_data['section-plans'];
+$datos = $mi_seccion['contenido'];
+// Definimos los grupos de planes
+$plans_group1 = ['plan_standard', 'plan_plus', 'plan_premium'];
+$plans_group2 = ['plan_metal', 'plan_ultra'];
 ?>
 <section id="section-plans">
-    <h2 class="static-title"><?php echo $datos['static_blocks']['title']; ?></h2>
+    <?php renderTitle(['title' => $datos['static_blocks']['title']], false, 'hero-content-title', 2); ?>
     <ul class="plan-container">
-        <li>
-            <?php renderPlan($datos['static_blocks']['plans']['plan_standard']); ?>
-        </li>
-        <li>
-            <?php renderPlan($datos['static_blocks']['plans']['plan_plus']); ?>
-        </li>
-        <li>
-            <?php renderPlan($datos['static_blocks']['plans']['plan_premium']); ?>
-        </li>
+        <?php renderPlanGroup($plans_group1, $datos); ?>
     </ul>
 
     <ul class="plan-container">
-       <li>
-            <?php renderPlan($datos['static_blocks']['plans']['plan_metal']); ?>
-        </li>
-        <li>
-            <?php renderPlan($datos['static_blocks']['plans']['plan_ultra']); ?>
-        </li>
+        <?php renderPlanGroup($plans_group2, $datos); ?>
     </ul>
 </section>
