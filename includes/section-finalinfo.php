7cebf81f277837ee50f1a0d8b74128e4d3a95358
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-26 12:54:40 +0100

    Update section-finalinfo.php

--- includes/section-finalinfo.php
+++ includes/section-finalinfo.php
@@ -5,9 +5,7 @@ $datos = $mi_seccion['contenido'];
 <section id="finalinfo">
     <?php renderTitle($datos['static_blocks'], false); ?>
 
-    <?php renderImage($datos['static_blocks'], false); ?>
+    <?php renderButton($datos['static_blocks']['button'], false); ?>
 
     <?php renderText($datos['static_blocks'], false); ?>
-
-    <?php renderButton($datos['static_blocks']['button'], false); ?>
 </section>
