d3285d477b8cca92bb5bbee4e409b12360b74889
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-30 13:40:36 +0200

    Update section-stokscommodities.php

--- includes/section-stokscommodities.php
+++ includes/section-stokscommodities.php
@@ -8,7 +8,7 @@ $inicial = $datos['section_config']['initial_state'];
     
     <?php renderText($datos['dynamic_blocks'][$inicial], true); ?>
 
-    <?php renderButton($datos['static_blocks']['button'], false, 'hidden-mobile'); ?>
+    <?php renderButton($datos['static_blocks']['button'], false, 'is-hidden-mobile'); ?>
 
     <?php renderImage($datos['dynamic_blocks'][$inicial], true); ?>
 
