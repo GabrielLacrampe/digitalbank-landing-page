c30f47dc8f6ab077b30881f31ba5c86ade3296dc
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-30 13:40:26 +0200

    Update section-links.php

--- includes/section-links.php
+++ includes/section-links.php
@@ -3,7 +3,7 @@ $mi_seccion = $web_data['section-links'];
 $datos = $mi_seccion['contenido']; 
 ?>
 <!-- En movil se transforman en desplegables, solo se ve el titulo-->
-<section id="section-links" class="bottom-links-container">
+<section id="section-links" class="links">
     <?php 
     renderListGroup($datos['static_blocks']['list_a']); 
     renderListGroup($datos['static_blocks']['list_b']); 
