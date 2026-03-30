c670c3b321b078537169ff7aea4c7a5817cff388
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-03-30 15:05:14 +0200

    Update section-links.php

--- includes/section-links.php
+++ includes/section-links.php
@@ -7,6 +7,14 @@ $datos = $mi_seccion['contenido'];
     <?php 
     renderListGroup($datos['static_blocks']['list_a']); 
     renderListGroup($datos['static_blocks']['list_b']); 
-    renderListGroup($datos['static_blocks']['list_c']); 
+    renderListGroup($datos['static_blocks']['list_c']);
+    renderListGroup($datos['static_blocks']['list_d']);
+    renderListGroup($datos['static_blocks']['list_e']);
+    renderListGroup($datos['static_blocks']['list_f']);
+    renderListGroup($datos['static_blocks']['list_g']);
+    renderListGroup($datos['static_blocks']['list_h']);
+    renderListGroup($datos['static_blocks']['list_i']);
+    renderListGroup($datos['static_blocks']['list_j']);
+    renderListGroup($datos['static_blocks']['list_k']);
     ?>
 </section>
