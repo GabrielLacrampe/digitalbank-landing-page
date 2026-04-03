17a76c3f2877fc10a0a395f2fcba7d13032fb538
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-04-03 15:07:06 +0200

    Update section-stokscommodities.php

--- includes/section-stokscommodities.php
+++ includes/section-stokscommodities.php
@@ -3,19 +3,19 @@ $mi_seccion = $web_data['section-stocks-commodities'];
 $datos = $mi_seccion['contenido']; 
 $inicial = $datos['section_config']['initial_state']; 
 ?>
-<section id="stocks-commodities" class="hero">
+<section id="stocks-commodities" class="main__section">
     <?php renderImage($datos['dynamic_blocks'][$inicial], true, 'background__image'); ?>
-    <div class="hero__logo"></div>
+    <div class="dymension__20"></div>
 
-    <div class="hero__inner hero__inner--static">
-        <?php renderTitle($datos['dynamic_blocks'][$inicial], true, 'hero__content'); ?>
+    <div class="dymension__80 section__static__content">
+        <?php renderTitle($datos['dynamic_blocks'][$inicial], true, 'light__content'); ?>
         
-        <?php renderText($datos['dynamic_blocks'][$inicial], true, 'hero__content'); ?>
+        <?php renderText($datos['dynamic_blocks'][$inicial], true, 'light__content'); ?>
 
-        <?php renderButton($datos['static_blocks']['button'], false, 'hero__content'); ?>
+        <?php renderButton($datos['static_blocks']['button'], false, 'light__content'); ?>
     </div>
 
-    <div class="hero__logo">
+    <div class="dymension__20">
         <?php renderControllers($datos['controllers']); ?>
         <script class="data-json" type="application/json">
             <?php echo json_encode($datos['dynamic_blocks']); ?>
