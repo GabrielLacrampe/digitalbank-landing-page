de6db02a38f87f87bf527a51a176d25054300be9
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-04-03 15:07:01 +0200

    Update section-safety.php

--- includes/section-safety.php
+++ includes/section-safety.php
@@ -2,17 +2,17 @@
 $mi_seccion = $web_data['section-safety']; 
 $datos = $mi_seccion['contenido']; 
 ?>
-<section id="safety" class="hero">
+<section id="safety" class="main__section">
     <?php renderImage($datos['static_blocks'], false, 'background__image'); ?>
-    <div class="hero__logo"></div>
+    <div class="dymension__20"></div>
     
-    <div class="hero__inner hero__inner--static">
-        <?php renderTitle($datos['static_blocks'], false, 'hero__content'); ?>
+    <div class="dymension__80 section__static__content">
+        <?php renderTitle($datos['static_blocks'], false, 'light__content'); ?>
 
-        <?php renderText($datos['static_blocks'], false, 'hero__content'); ?>
+        <?php renderText($datos['static_blocks'], false, 'light__content'); ?>
     
-        <?php renderButton($datos['static_blocks']['button'], false, 'hero__content'); ?>
+        <?php renderButton($datos['static_blocks']['button'], false, 'light__content'); ?>
     </div>
 
-    <div class="hero__logo"></div>
+    <div class="dymension__20"></div>
 </section>
