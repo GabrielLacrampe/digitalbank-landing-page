36e05626a874490f3820cef5261dfb32d0257962
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-04-03 15:06:56 +0200

    Update section-hero.php

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -2,23 +2,23 @@
 $mi_seccion = $web_data['section-hero']; 
 $datos = $mi_seccion['contenido']; 
 ?>
-<section id="main" class="hero">        
+<section id="main" class="main__section">        
     <?php renderImage($datos['static_blocks']['images']['image_a'], false, 'hero__background'); ?>
     
-    <div class="hero__logo"></div>
+    <div class="dymension__20"></div>
 
-    <div class="hero__inner">
-        <?php renderTitle($datos['static_blocks']['group_a'], false, 'hero__content hero__group hero__group--primary'); ?>
-        <?php renderText($datos['static_blocks']['group_a'], false, 'hero__content hero__group hero__group--primary'); ?>        
-        <?php renderButton($datos['static_blocks']['group_a']['button'], false, 'hero__content hero__group hero__group--primary'); ?>
+    <div class="dymension__80">
+        <?php renderTitle($datos['static_blocks']['group_a'], false, 'light__content hero__group hero__group--primary'); ?>
+        <?php renderText($datos['static_blocks']['group_a'], false, 'light__content hero__group hero__group--primary'); ?>        
+        <?php renderButton($datos['static_blocks']['group_a']['button'], false, 'light__content hero__group hero__group--primary'); ?>
 
-        <?php renderTitle($datos['static_blocks']['group_b'], false, 'hero__content hero__group hero__group--secondary'); ?>
-        <?php renderText($datos['static_blocks']['group_b'], false, 'hero__content hero__group hero__group--secondary'); ?>
-        <?php renderButton($datos['static_blocks']['group_b']['button'], false, 'hero__content hero__group hero__group--secondary'); ?>
+        <?php renderTitle($datos['static_blocks']['group_b'], false, 'light__content hero__group hero__group--secondary'); ?>
+        <?php renderText($datos['static_blocks']['group_b'], false, 'light__content hero__group hero__group--secondary'); ?>
+        <?php renderButton($datos['static_blocks']['group_b']['button'], false, 'light__content hero__group hero__group--secondary'); ?>
 
         <?php renderImage($datos['static_blocks']['images']['image_b'], false, 'hero__image hero__image--left'); ?>
         <?php renderImage($datos['static_blocks']['images']['image_c'], false, 'hero__image hero__image--right'); ?>
     </div>
 
-    <div class="hero__logo"></div>     
+    <div class="dymension__20"></div>
 </section>
