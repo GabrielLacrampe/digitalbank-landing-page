78ab01415ff3bd13e048a784778b2066d90c6fcf
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-04-03 14:03:30 +0200

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,15 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Principal</title>
-<link rel="stylesheet" href="../assets/css/styles.css">
+<link rel="stylesheet" href="../assets/css/variables.css">
+<link rel="stylesheet" href="../assets/css/components.css">
+<link rel="stylesheet" href="../assets/css/layouts.css">
+<link rel="stylesheet" href="../assets/css/animations.css">
+<link rel="stylesheet" href="../assets/css/mediaqueries.css">
+<link rel="stylesheet" href="../assets/css/utilities.css">
+<link rel="stylesheet" href="../assets/css/basestyles.css">
+<link rel="stylesheet" href="../assets/css/reset-normalization.css">
+
 <?php require_once '../config/db.php'; ?>
 <?php require_once '../config/queries.php'; ?>
 <?php require_once '../includes/components/link.php'; ?>
