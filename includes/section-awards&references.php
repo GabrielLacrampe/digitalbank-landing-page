8510350e948d24d861a4ba4df1df2263eed2cf76
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-04-25 15:51:02 +0200

    rework of section-awards
    fixing bad layout

--- includes/section-awards&references.php
+++ includes/section-awards&references.php
@@ -3,21 +3,29 @@
     $mi_seccion = $web_data['section-awards_references']; 
     $datos = $mi_seccion['contenido']; 
 ?>
-<section id="awards-references" class="main__section">
+<section id="awards-references" class="main__section awards-references">
     <?php renderTitle($datos['static_blocks']); ?>
-    
-    <?php renderText($datos['static_blocks']['award_a']); ?>
-    <?php renderImage($datos['static_blocks']['award_a']); ?>
 
-    <?php renderText($datos['static_blocks']['award_b']); ?>
-    <?php renderImage($datos['static_blocks']['award_b']); ?>
+    <div class="awards-references__grid">
+        <?php
+            $awards = [
+                'award_a',
+                'award_b',
+                'award_c',
+                'award_d',
+                'award_e',
+            ];
 
-    <?php renderText($datos['static_blocks']['award_c']); ?>
-    <?php renderImage($datos['static_blocks']['award_c']); ?>
-    
-    <?php renderText($datos['static_blocks']['award_d']); ?>
-    <?php renderImage($datos['static_blocks']['award_d']); ?>
-    
-    <?php renderText($datos['static_blocks']['award_e']); ?>
-    <?php renderImage($datos['static_blocks']['award_e']); ?>
+            foreach ($awards as $award) :
+                $item = $datos['static_blocks'][$award] ?? null;
+                if (empty($item)) continue;
+        ?>
+            <article class="awards-references__item">
+                <div class="awards-references__media">
+                    <?php renderImage($item, 'awards-references__image'); ?>
+                </div>
+                <?php renderText($item, 'awards-references__text'); ?>
+            </article>
+        <?php endforeach; ?>
+    </div>
 </section>
